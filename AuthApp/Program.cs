7913b344a19c28c4f3536160eeea4fee6439f6d9
Vladimir <vladim.ignatiew2010@yandex.ru>
2023-11-24 22:05:02 +0400

    update console test

---
+++ AuthApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using AuthApp;
+
+var userOk = new User()
+{
+    Login = "user",
+    Password = "12345",
+};
+
+var userBad = new User()
+{
+    Login = "admin",
+    Password = "123",
+};
+
+var auth = new LoggerAuth(new SimpleAuth(), new LogToConsole());
+
+Console.WriteLine(auth.IsAuth(userOk) ? "OK" : "ERROR");
+Console.WriteLine(auth.IsAuth(userBad) ? "OK" : "ERROR");
